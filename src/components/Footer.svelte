40b044192cbd634a883fe86a6ad29f9f86a8185f
sam <boadusamuel@gmail.com>
2023-10-10 07:39:22 +0000

    fixed action button not working

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,4 +1,5 @@
 <footer
+id="contact"
     class="py-20 sm:py-32 bg-black border-t border-solid border-violet-950 flex flex-col gap-4 sm:gap-8 justify-center items-center"
 >
     <p class="px-4 py-2 bg-white text-slate-950 font-medium">
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,27 +13,26 @@
 </script>
 
 <div
-  class="relative flex flex-col max-w-[1400px] mx-auto w-full text-sm sm:text-base min-h-screen"
+  class="container relative flex flex-col max-w-[1400px] mx-auto w-full text-sm sm:text-base min-h-screen"
 >
-  <div
-    class={"fixed bottom-0 w-full duration-200 flex p-10 z-[10] " +
-      (y > 0
-        ? " opacity-full pointer-events-auto"
-        : " pointer-events-none opacity-0")}
-  >
-    <button
-      on:click={goTop}
-      class="ml-auto rounded-full bg-slate-900 text-violet-400 px-3 sm:px-4
-    hover:bg-slate-800 cursor-pointer"
+<div
+        class={"fixed bottom-0 w-full duration-200 flex p-10 z-[10] " +
+            (y > 0
+                ? "opacity-full pointer-events-auto"
+                : "pointer-events-none opacity-0")}
     >
-      <i class="fa-solid fa-arrow-up grid place-items-center aspect-square" />
-    </button>
-  </div>
+        <button
+            on:click={goTop}
+            class="ml-auto rounded-full bg-slate-900 text-violet-400 px-3 sm:px-4 hover:bg-slate-800 cursor-pointer aspect-square grid place-items-center"
+        >
+            <i class="fa-solid fa-arrow-up" />
+        </button>
+    </div>
 
-<Header y={y} />
+<Header y={y}  {innerHeight}/>
 <slot />
 <Footer />
 </div>
 
 
-<svelte:window binf:scroll={y} bind:innerHeight bind:innerWidth />
+<svelte:window bind:scrollY={y} bind:innerHeight bind:innerWidth />
